906c9a1c2ff768387081ecf486fcee0c5f780eaa
Song <w2033@e-mirim.hs.kr>
2021-07-30 11:10:26 +0900

    Edit activity_main.xml (TextView 추가) Commit

--- app/src/main/java/kr/hs/emirim/w2033/ex13_9_11/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/w2033/ex13_9_11/MainActivity.java
@@ -7,9 +7,11 @@ import android.os.SystemClock;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar seek1, seek2;
+    TextView tv1, tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,20 +20,21 @@ public class MainActivity extends AppCompatActivity {
         setTitle("Tread 기본");
         seek1 = findViewById(R.id.seek1);
         seek2 = findViewById(R.id.seek2);
+        tv1 = findViewById(R.id.tv1);
         Button btnStart = findViewById(R.id.btn_start);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Thread thread1 = new Thread(){
+                new Thread(){
                     @Override
                     public void run() {
                         for(int i = seek1.getProgress(); i < 100; i+=2){
                             seek1.setProgress(seek1.getProgress()+2);
+
                             SystemClock.sleep(100);
                         }
                     }
-                };
-                thread1.start();
+                }.start();
 
                 Thread thread2 = new Thread(){
                     @Override
